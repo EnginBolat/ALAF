22850e341de9991aff5b7935ca6f6518c519a573
Engin Bolat <engi.bolat@gmail.com>
2024-05-16 00:39:59 +0300

    added formik form for the imputs

--- src/pages/AddAdress/_layout.tsx
+++ src/pages/AddAdress/_layout.tsx
@@ -1,16 +1,77 @@
 import React from "react";
-import { SafeAreaView, ScrollView } from "react-native";
-import { PrimaryButton } from "../../components";
+import { SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Divider, PrimaryButton, PrimaryInput } from "../../components";
+import * as yup from 'yup';
+import { Formik } from "formik";
 
 
 export default function AddAdress() {
-    return <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'space-between' }}>
-        <SafeAreaView style={{ flex: 1, }}>
-            <PrimaryButton
-                title="Kaydet"
-                onPress={() => { }}
-                disabled={true}
-            />
-        </SafeAreaView>
-    </ScrollView>
+
+    let formScheme = yup.object({
+        adressTitle: yup.string().min(1, 'Daha uzun bir başlık gerekli').max(50, 'Daha kısa bir başlık gerekli').required(),
+        adressDescription: yup.string().min(1, 'Daha uzun bir açıklama gerekli').max(50, 'Daha kısa bir açıklama gerekli').required(),
+    });
+
+
+    return <SafeAreaView style={{ flex: 1, }}>
+        <Formik
+            initialValues={{
+                adressTitle: '',
+                adressDescription: ''
+            }}
+            validationSchema={formScheme}
+            onSubmit={values => console.log(values)}
+        >
+            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                <View style={{ flex: 1, }}>
+                    <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'space-between' }}>
+                        <View style={[styles.pagePadding, styles.innerContainer]}>
+                            <PrimaryInput
+                                label='Adres başlığı (Ev, işyeri vs.)'
+                                onChangeText={handleChange('adressTitle')}
+                                value={values.adressTitle}
+                            />
+                            <View style={{ paddingVertical: 12, }}>
+                                <PrimaryInput
+                                    label='İl'
+                                    onChangeText={handleChange('adressTitle')}
+                                    value={values.adressTitle}
+                                />
+                            </View>
+                            <PrimaryInput
+                                label='Adres Detayı'
+                                onChangeText={handleChange('adressDescription')}
+                                value={values.adressDescription}
+                            />
+                            {errors.adressDescription && touched.adressDescription ? <Text>{errors.adressDescription}</Text> : null}
+                        </View>
+                    </ScrollView>
+                    <View>
+                        <Divider />
+                        <View style={[styles.pagePadding, styles.buttonStyle]}>
+                            <PrimaryButton
+                                title="Yeni Kayıt Ekle"
+                                onPress={handleSubmit}
+                                disabled={values.adressDescription.length > 1 && values.adressTitle.length > 1 ? false : true}
+                            />
+                        </View>
+                    </View>
+                </View>
+            )}
+        </Formik>
+    </SafeAreaView>
 }
+
+
+const styles = StyleSheet.create({
+    innerContainer: {
+        flex: 1,
+        backgroundColor: '#0000',
+    },
+    pagePadding: {
+        paddingHorizontal: 20,
+    },
+    buttonStyle: {
+        paddingTop: 15,
+    },
+})
