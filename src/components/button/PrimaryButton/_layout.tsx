0b80b76bd96609bc63d2de7126f5f8a868b548ec
Engin Bolat <engi.bolat@gmail.com>
2024-05-15 23:18:14 +0300

    added disabled and testId varaibles for the testing and the disabled theme

--- src/components/button/PrimaryButton/_layout.tsx
+++ src/components/button/PrimaryButton/_layout.tsx
@@ -4,12 +4,22 @@ import { StyleSheet, Text, TouchableOpacity } from "react-native";
 type PrimaryButtonProps = {
     onPress: () => void;
     title: string;
+    disabled?: boolean;
+    testID: string
 };
 
-const PrimaryButton: React.FC<PrimaryButtonProps> = ({ onPress, title }) => {
+const PrimaryButton: React.FC<PrimaryButtonProps> = ({ onPress, title, testID, disabled = false }) => {
     return (
-        <TouchableOpacity onPress={onPress} style={styles.container}>
-            <Text style={styles.text}>{title}</Text>
+        <TouchableOpacity onPress={onPress} style={disabled ? styles.disabledContainer : styles.container} testID={testID} disabled={disabled}>
+            <Text
+                style={[
+                    styles.text,
+                    disabled
+                        ? { color: '#AAB5C1' }
+                        : { color: 'white' }]}
+            >
+                {title}
+            </Text>
         </TouchableOpacity>
     );
 };
@@ -17,6 +27,12 @@ const PrimaryButton: React.FC<PrimaryButtonProps> = ({ onPress, title }) => {
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#019693',
+        paddingVertical: 18,
+        borderRadius: 6,
+        alignItems: 'center',
+    },
+    disabledContainer: {
+        backgroundColor: '#EEF0F4',
         paddingVertical: 18,
         borderRadius: 6,
         alignItems: 'center',
