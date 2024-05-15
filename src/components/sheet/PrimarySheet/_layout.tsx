081977e76a948bbb6aa34343957071c0d1e3252c
Engin Bolat <engi.bolat@gmail.com>
2024-05-16 01:20:59 +0300

    Created PrimarySheet

---
+++ src/components/sheet/PrimarySheet/_layout.tsx
@@ -0,0 +1,71 @@
+import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
+import { IcSuccess } from "../../../assets";
+import { StyleSheet, Text, View } from "react-native";
+
+type PrimarySheetProps = {
+    bottomSheetModalRef: any,
+    snapPoints: any
+    handleSheetChanges: any,
+    title: string,
+    isSuccess?: boolean;
+    isHaveIcon?: boolean;
+}
+
+const PrimarySheet: React.FC<PrimarySheetProps> = ({
+    bottomSheetModalRef,
+    snapPoints,
+    handleSheetChanges,
+    title,
+    isSuccess,
+    isHaveIcon
+}) => {
+    return <BottomSheetModal
+        ref={bottomSheetModalRef}
+        enablePanDownToClose={false}
+        index={1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+        handleIndicatorStyle={styles.bsIndicator}
+        containerStyle={styles.bsBackground}
+    >
+        <BottomSheetView
+            style={styles.bsContentContainer}>
+            {isHaveIcon && <View style={styles.bsIconContainer}>
+                {isSuccess
+                    ? <IcSuccess fill='#019693' />
+                    // Buraya getirelecek error iconu ile error handle edilebilir.
+                    : <IcSuccess fill='#019693' />}
+            </View>}
+            <Text style={styles.bsTitle}>{title}</Text>
+
+        </BottomSheetView>
+    </BottomSheetModal>;
+}
+
+
+const styles = StyleSheet.create({
+    bsBackground: {
+        backgroundColor: 'rgba(0,0,0,0.50)'
+    },
+    bsIndicator: {
+        backgroundColor: '#EEF0F4'
+    },
+    bsContentContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bsIconContainer: {
+        padding: 16,
+        backgroundColor: '#E3F7F7',
+        borderRadius: 100
+    },
+    bsTitle: {
+        fontWeight: '500',
+        fontSize: 16,
+        lineHeight: 16,
+        paddingTop: 16,
+    }
+})
+
+export default PrimarySheet;
