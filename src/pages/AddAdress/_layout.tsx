6c41fc7e1745129026a9bfc2403582cf72c68cd4
Engin Bolat <engi.bolat@gmail.com>
2024-05-16 01:16:58 +0300

    Added bottom sheet controllers

--- src/pages/AddAdress/_layout.tsx
+++ src/pages/AddAdress/_layout.tsx
@@ -1,66 +1,102 @@
-import React from "react";
-import { SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
-import { Divider, PrimaryButton, PrimaryInput } from "../../components";
+import React, { useCallback, useMemo, useRef } from "react";
+import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
+
 import * as yup from 'yup';
 import { Formik } from "formik";
+import { BottomSheetModal, BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { BottomButtonLayout, PrimaryInput, PrimarySheet } from "../../components";
 
 
 export default function AddAdress() {
 
+    // ref
+    const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+    // variables
+    const snapPoints = useMemo(() => ['25%', '25%'], []);
+
+    // callbacks
+    const handlePresentModalPress = useCallback(() => {
+        bottomSheetModalRef.current?.present();
+    }, []);
+
+    const handleSheetChanges = useCallback((index: number) => {
+        console.log('handleSheetChanges', index);
+    }, []);
+
+
+    // Form Scheme
     let formScheme = yup.object({
         adressTitle: yup.string().min(1, 'Daha uzun bir başlık gerekli').max(50, 'Daha kısa bir başlık gerekli').required(),
         adressDescription: yup.string().min(1, 'Daha uzun bir açıklama gerekli').max(50, 'Daha kısa bir açıklama gerekli').required(),
     });
 
 
-    return <SafeAreaView style={{ flex: 1, }}>
-        <Formik
-            initialValues={{
-                adressTitle: '',
-                adressDescription: ''
-            }}
-            validationSchema={formScheme}
-            onSubmit={values => console.log(values)}
-        >
-            {({ handleChange, handleSubmit, values, }) => (
-                <View style={{ flex: 1, }}>
-                    <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'space-between' }}>
-                        <View style={[styles.pagePadding, styles.innerContainer]}>
-                            <PrimaryInput
-                                label='Adres başlığı (Ev, işyeri vs.)'
-                                onChangeText={handleChange('adressTitle')}
-                                value={values.adressTitle}
-                            />
-                            <View style={{ paddingVertical: 12, }}>
+    return <BottomSheetModalProvider>
+        <SafeAreaView style={{ flex: 1, }}>
+            <Formik
+                initialValues={{
+                    adressTitle: '',
+                    adressDescription: ''
+                }}
+                validationSchema={formScheme}
+                onSubmit={values => {
+                    console.log(values)
+                    handlePresentModalPress();
+                }}
+            >
+                {({ handleChange, handleSubmit, values, }) => (
+                    <View style={{ flex: 1, }}>
+                        <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'space-between' }}>
+                            <View style={[styles.pagePadding, styles.innerContainer]}>
                                 <PrimaryInput
-                                    label='İl'
+                                    label='Adres başlığı (Ev, işyeri vs.)'
                                     onChangeText={handleChange('adressTitle')}
                                     value={values.adressTitle}
                                 />
+                                <View style={{ paddingVertical: 12, }}>
+                                    <PrimaryInput
+                                        label='İl'
+                                        onChangeText={handleChange('adressTitle')}
+                                        value={values.adressTitle}
+                                    />
+                                </View>
+                                <PrimaryInput
+                                    label='Adres Detayı'
+                                    onChangeText={handleChange('adressDescription')}
+                                    value={values.adressDescription}
+                                />
                             </View>
-                            <PrimaryInput
-                                label='Adres Detayı'
-                                onChangeText={handleChange('adressDescription')}
-                                value={values.adressDescription}
-                            />
-                        </View>
-                    </ScrollView>
-                    <View>
-                        <Divider />
-                        <View style={[styles.pagePadding, styles.buttonStyle]}>
-                            <PrimaryButton
-                                title="Yeni Kayıt Ekle"
-                                onPress={handleSubmit}
-                                disabled={values.adressDescription.length > 1 && values.adressTitle.length > 1 ? false : true}
-                            />
-                        </View>
+                        </ScrollView>
+                        {/* <View>
+                            <Divider />
+                            <View style={[styles.pagePadding, styles.buttonStyle]}>
+                                <PrimaryButton
+                                    title="Kaydet"
+                                    onPress={handleSubmit}
+                                    disabled={values.adressDescription.length > 1 && values.adressTitle.length > 1 ? false : true}
+                                />
+                            </View>
+                        </View> */}
+                        <BottomButtonLayout
+                            title="Kaydet"
+                            onPress={handleSubmit}
+                            disabled={values.adressDescription.length > 1 && values.adressTitle.length > 1 ? false : true}
+                        />
                     </View>
-                </View>
-            )}
-        </Formik>
-    </SafeAreaView>
+                )}
+            </Formik>
+            <PrimarySheet
+                isHaveIcon={true}
+                isSuccess={true}
+                title="Adresin başarıyla kaydedildi!"
+                bottomSheetModalRef={bottomSheetModalRef}
+                snapPoints={snapPoints}
+                handleSheetChanges={handleSheetChanges}
+            />
+        </SafeAreaView>
+    </BottomSheetModalProvider>
 }
-
 
 const styles = StyleSheet.create({
     innerContainer: {
@@ -69,8 +105,5 @@ const styles = StyleSheet.create({
     },
     pagePadding: {
         paddingHorizontal: 20,
-    },
-    buttonStyle: {
-        paddingTop: 15,
     },
 })
