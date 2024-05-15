c0e86f8fcbd8605084e2a24353f8f986960e704d
Engin Bolat <engi.bolat@gmail.com>
2024-05-15 23:39:49 +0300

    added animated text for the label

--- src/components/input/PrimaryInput/_layout.tsx
+++ src/components/input/PrimaryInput/_layout.tsx
@@ -1,18 +1,89 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useRef, useState } from "react";
+import { Animated, StyleSheet, Text, TextInput, View } from "react-native";
 
 type PrimaryInputProps = {
-    label: String;
+    label: string;
 }
 
 
 const PrimaryInput: React.FC<PrimaryInputProps> = ({ label }) => {
-    return <View>
-        <Text>{label}</Text>
-    </View>
-}
+    const [text, setText] = useState('');
+    const floatingLabelAnimation = useRef(
+        new Animated.Value(text ? 1 : 0),
+    ).current;
 
-const styles = StyleSheet.create({})
+    const handleFocus = () => {
+        // Animate the label up and reduce its size when input is focus
+        Animated.timing(floatingLabelAnimation, {
+            toValue: 1,
+            duration: 150,
+            useNativeDriver: false,
+        }).start();
+    };
+
+    const handleBlur = () => {
+        // If the input is empty, animate the floating label back to its original position
+        if (!text) {
+            Animated.timing(floatingLabelAnimation, {
+                toValue: 0,
+                duration: 150,
+                useNativeDriver: false,
+            }).start();
+        }
+    };
+
+    // Define animated styles for the floating label
+    const floatingLabelStyle = {
+        top: floatingLabelAnimation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [10, -5], // top value
+        }),
+        fontSize: floatingLabelAnimation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [16, 12], // font size
+        }),
+    };
+
+    return (
+        <View style={styles.container}>
+            <Animated.Text style={[styles.label, floatingLabelStyle]}>
+                {label}
+            </Animated.Text>
+            <TextInput
+                cursorColor={'#6F6085'}
+                selectionHandleColor={''}
+                style={styles.inputText}
+                value={text}
+                onChangeText={val => setText(val)}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#E6E9EE',
+        backgroundColor: '#FCFCFD',
+        paddingVertical: 21,
+        paddingHorizontal: 16,
+        borderRadius: 8,
+    },
+    label: {
+        fontWeight: '500',
+        fontSize: 14,
+        lineHeight: 18,
+        color: '#6F6085',
+    },
+    inputText: {
+        fontWeight: '300',
+        fontSize: 14,
+        lineHeight: 18
+    },
+})
 
 
 export default PrimaryInput;
