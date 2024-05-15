4fa4a4e0855fc68efa78fccc5e7de58feef4be66
Engin Bolat <engi.bolat@gmail.com>
2024-05-15 15:34:13 +0300

    AdressList page created

---
+++ src/pages/AdressList/_layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+export default function AdressList(){
+    return <SafeAreaView>
+        <Text>Merhaba, Dünya!</Text>
+    </SafeAreaView>
+}
