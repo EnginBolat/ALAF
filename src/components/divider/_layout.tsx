e69603c96496ed4e6a1b5817e0357b72d965cc8f
Engin Bolat <engi.bolat@gmail.com>
2024-05-15 15:51:57 +0300

    divider component created and added background color

---
+++ src/components/divider/_layout.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, View } from "react-native";
+
+export default function Divider() {
+    return <View style={styles.container} />
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#FFFFFF',
+    }
+})
