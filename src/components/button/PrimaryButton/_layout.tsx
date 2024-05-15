468a41f815e1081cf26cc820db3fbe6ac32afc1f
Engin Bolat <engi.bolat@gmail.com>
2024-05-15 15:33:45 +0300

    created primarybutton template

---
+++ src/components/button/PrimaryButton/_layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+export default function PrimaryButton() {
+    <TouchableOpacity onPress={() => { }}>
+        <Text>Button</Text>
+    </TouchableOpacity>
+}
