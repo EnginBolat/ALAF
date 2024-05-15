357428784919f98d31b78bcc78bc890f30f2a8dc
Engin Bolat <engi.bolat@gmail.com>
2024-05-16 01:19:14 +0300

    added sucess icon export

--- src/assets/ic/index.ts
+++ src/assets/ic/index.ts
@@ -1,2 +1,3 @@
 export { default as IcLocation } from './ic_location.svg'
 export { default as IcChevronRight } from './ic_chevron_right.svg';
+export { default as IcSuccess } from './ic_success.svg';
