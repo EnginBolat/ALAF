c5b5e09805164ddc50f55d87b3a0944116ad6c0b
Engin Bolat <engi.bolat@gmail.com>
2024-05-16 15:01:30 +0300

    fetchCities action created

--- src/redux/actions/cities.actions.ts
+++ src/redux/actions/cities.actions.ts
@@ -3,12 +3,11 @@ import axios, { HttpStatusCode } from "axios";
 import { API_URL } from '.env';
 
 export const fetchCities = createAsyncThunk(
-    'product/fetchProducts',
+    'cities/fetchCities',
     async () => {
         try {
-            const { data, status } = await axios.get(`${API_URL}/cities`)
+            const { data, status } = await axios.get(`https://6645cd02b8925626f8933d3a.mockapi.io/cities`)
             if (status === HttpStatusCode.Ok) {
-                console.log(data);
                 return data;
             }
             throw new Error(`We can't reach our servers please try again later.`)
