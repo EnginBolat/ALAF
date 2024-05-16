00ed99d084cdfe641a9637cff465beae07cc91bc
Engin Bolat <engi.bolat@gmail.com>
2024-05-16 11:36:38 +0300

    chevron left export added

--- src/assets/ic/index.ts
+++ src/assets/ic/index.ts
@@ -1,3 +1,4 @@
 export { default as IcLocation } from './ic_location.svg'
 export { default as IcChevronRight } from './ic_chevron_right.svg';
+export { default as IcChevronLeft } from './ic_chevron_left.svg';
 export { default as IcSuccess } from './ic_success.svg';
