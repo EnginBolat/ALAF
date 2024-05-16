9c8905732230d13ece6aad66dcb49b21792735f1
Engin Bolat <engi.bolat@gmail.com>
2024-05-16 15:02:56 +0300

    adress and cities recucers added

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { citiesSlice } from '../reducer'
+import { citiesReducer, adressReducer } from '../reducer'
 
 export const store = configureStore({
     reducer: {
-        cities: citiesSlice,
+        cities: citiesReducer,
+        adress: adressReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 })
 export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
