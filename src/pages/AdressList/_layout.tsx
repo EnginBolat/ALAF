501b90422e8d9a15ebafe47292389eb49ebd0b53
Engin Bolat <engi.bolat@gmail.com>
2024-05-16 15:00:58 +0300

    redux integration

--- src/pages/AdressList/_layout.tsx
+++ src/pages/AdressList/_layout.tsx
@@ -1,119 +1,87 @@
-import React from "react";
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
-import { AdressContainer, BottomButtonLayout, Divider } from "../../components";
+import React, { useEffect } from "react";
+import { Dimensions, SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
-
-const data = [
-    {
-        id: 1,
-        adressTitle: "İkamet Adresim",
-        adressDetails: "Adres detayı, lor",
-        currentAdress: "Üsküdar/İstanbul",
-    },
-    {
-        id: 2,
-        adressTitle: "İkamet Adresim",
-        adressDetails: "Adres detayı, lor",
-        currentAdress: "Üsküdar/İstanbul",
-    },
-    {
-        id: 3,
-        adressTitle: "İkamet Adresim",
-        adressDetails: "Adres detayı, lor",
-        currentAdress: "Üsküdar/İstanbul",
-    },
-]
+import { AdressContainer, BottomButtonLayout, ComponentTitle, Divider, ErrorText, Loading } from "../../components";
+import { AppDispatch, RootState, adressList } from "../../redux";
+const { height } = Dimensions.get('window');
 
 
 export default function AdressList({ navigation }) {
 
+    const { addresses, loading, error, } = useSelector((state: RootState) => state.adress);
+    const dispatch = useDispatch<AppDispatch>()
+    useEffect(() => { dispatch(adressList()) }, [])
+
+    if (loading) { return < Loading /> }
+    else if (error) { return <ErrorText error={error} /> }
+
     function handleAddNewRecordButton() { navigation.navigate('AddAdress'); }
-    
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.innerContainer}>
-
-                <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.scrollViewContentContainer}>
-                    <View style={styles.pagePadding}>
-                        {/* Title */}
-                        <Text style={styles.titleStyle}>Kayıtlı Adresler</Text>
-                        <View style={{ height: 10 }} />
-                        {/* Information Container */}
-                        <View style={styles.adressListContainer}>
-                            {data.map((e) => {
-                                return (
-                                    e.id == 2
-                                        ? <View>
-                                            <View style={{ paddingVertical: 16 }}>
-                                                <Divider />
-                                            </View>
-                                            <AdressContainer
-                                                key={e.id}
-                                                adressTitle={e.adressTitle}
-                                                adressDetails={e.adressDetails}
-                                                currentAdress={e.currentAdress}
-                                                onPress={() => { }}
-                                            />
-                                            <View style={{ paddingVertical: 16 }}>
-                                                <Divider />
-                                            </View>
-                                        </View>
-                                        : <AdressContainer
-                                            key={e.id}
-                                            adressTitle={e.adressTitle}
-                                            adressDetails={e.adressDetails}
-                                            currentAdress={e.currentAdress}
+                <View style={styles.pagePadding}>
+                    {/* Title */}
+                    <ComponentTitle title="Kayıtlı Adresler" />
+                    <View style={{ height: 10 }} />
+                    {/* Information Container */}
+                    <View style={styles.adressListContainer}>
+                        <ScrollView showsVerticalScrollIndicator={false}>
+                            {addresses?.map((adress) => {
+                                return (Number(adress.id) % 2 == 0)
+                                    ? <View>
+                                        <Divider />
+                                        <AdressContainer
+                                            key={adress.id}
+                                            adressTitle={adress.adressTitle}
+                                            adressDetails={adress.adressDescription}
+                                            currentAdress={adress.currentAdress}
                                             onPress={() => { }}
                                         />
-                                )
+                                        <Divider />
+                                    </View>
+                                    : <AdressContainer
+                                        key={adress.id}
+                                        adressTitle={adress.adressTitle}
+                                        adressDetails={adress.adressDescription}
+                                        currentAdress={adress.currentAdress}
+                                        onPress={() => { }}
+                                    />
                             })}
-                        </View>
+                        </ScrollView>
                     </View>
-                </ScrollView>
-                {/* Button Area */}
-                <BottomButtonLayout
-                    title="Yeni Kayıt Ekle"
-                    onPress={handleAddNewRecordButton}
-                />
+                </View>
+
             </View>
-        </SafeAreaView>
+            <BottomButtonLayout
+                title="Yeni Kayıt Ekle"
+                onPress={handleAddNewRecordButton}
+            />
+        </SafeAreaView >
     )
 }
 
 
 const styles = StyleSheet.create({
-    scrollViewContentContainer: {
-        flex: 1,
-        justifyContent: 'space-between'
-    },
     container: {
         flex: 1,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        justifyContent: 'space-between',
     },
     pagePadding: {
         paddingHorizontal: 20,
     },
     innerContainer: {
-        flex: 1,
         backgroundColor: '#0000',
-        justifyContent: 'space-between',
-    },
-    buttonStyle: {
-        paddingTop: 15,
-    },
-    titleStyle: {
-        fontSize: 14,
-        lineHeight: 18,
-        fontWeight: '500',
-        color: '#3D2852',
-        paddingTop: 30,
     },
     adressListContainer: {
-        paddingVertical: 20,
+        paddingTop: 20,
         paddingHorizontal: 16,
         borderWidth: 1,
         borderRadius: 8,
         borderStyle: 'solid',
         borderColor: '#EEF0F4',
+        maxHeight: height * 0.65
     }
 })
