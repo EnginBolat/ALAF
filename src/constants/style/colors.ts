73f710e143e83a5a4e710a633b8ec43364e40048
Engin Bolat <engi.bolat@gmail.com>
2024-05-17 13:01:59 +0300

    added colors the private file

---
+++ src/constants/style/colors.ts
@@ -0,0 +1,16 @@
+
+const Colors = {
+    button: "#019693",
+    primaryBorder: "#EEF0F4",
+    secondaryBorder: "#E6E9EE",
+    white: "#fff",
+    primaryTitle: "#000000",
+    secondaryTitle: "#6F6085",
+    thirdTitle: "#3D2852",
+    background: "#ffff",
+    primaryIcon: "#450D87",
+    primaryIconBackground: "#F1EEF7",
+    success: "#E3F7F7",
+}
+
+export default Colors;
