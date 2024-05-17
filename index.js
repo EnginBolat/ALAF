9096d51604840f165ac6a41db71d08c9390f3135
Engin Bolat <engi.bolat@gmail.com>
2024-05-17 13:38:01 +0300

    added i18n config file import

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import './src/core/i18n/i18n.config';
 
 AppRegistry.registerComponent(appName, () => App);
