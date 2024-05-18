47de059f2bebac9ed41fdbf0908323b256f21871
Engin Bolat <engi.bolat@gmail.com>
2024-05-18 13:11:39 +0300

    created deleteAdress

--- src/redux/actions/adress.actions.ts
+++ src/redux/actions/adress.actions.ts
@@ -7,9 +7,9 @@ export const adressList = createAsyncThunk(
     'adress/list',
     async () => {
         try {
-            const { data, status } = await axios.get(`${API_URL}/adressList`)
+            const { data, status } = await axios.get(`${API_URL}/adressList`) as { data: Array<Adress>, status: HttpStatusCode }
             if (status === HttpStatusCode.Ok) {
-                return data;
+                return data.filter((address) => address.isDeleted === false);
             }
             throw new Error(`We can't reach our servers please try again later.`)
         } catch (error) {
@@ -32,6 +32,7 @@ export const addAdress = createAsyncThunk(
 
             })
             if (status === HttpStatusCode.Created) {
+                if (__DEV__) console.log(`Created New Record`);
                 return data;
             }
             throw new Error(`We can't reach our servers please try again later.`)
@@ -41,3 +42,30 @@ export const addAdress = createAsyncThunk(
         }
     }
 )
+
+
+/**
+ * 
+ * Performs a soft delete of an address by setting its `isDeleted` property to `true`.
+ * Triggers the `addressList` thunk to reload the address list after a successful update  
+ * 
+ */
+export const deleteAdress = createAsyncThunk(
+    'adress/delete',
+    async ({ id, isDeleted }: { id: string, isDeleted: boolean }, thunkAPI) => {
+        try {
+            const { status } = await axios.patch(`${API_URL}/adressList/${id}`, {
+                isDeleted: isDeleted
+            });
+            if (status === 200) {
+                if (__DEV__) console.log(`Deleted Record Id:${id}`);
+                thunkAPI.dispatch(adressList());
+                return { id, isDeleted };
+            }
+            throw new Error(`We can't reach our servers please try again later.`);
+        } catch (error) {
+            console.log(error);
+            throw new Error(`We can't reach our servers please try again later.`);
+        }
+    }
+);
