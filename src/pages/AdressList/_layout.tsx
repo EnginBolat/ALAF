fde8796b3ac54e5d3882497fb8468db745fe3cb8
Engin Bolat <engi.bolat@gmail.com>
2024-05-18 13:11:29 +0300

    added deleteAdress action and swipable

--- src/pages/AdressList/_layout.tsx
+++ src/pages/AdressList/_layout.tsx
@@ -4,8 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useFocusEffect } from "@react-navigation/native";
 import { useTranslation } from 'react-i18next';
 
-import { AdressContainer, BaseView, BottomButtonLayout, ComponentTitle, Divider, ErrorText, Loading } from "../../components";
-import { AppDispatch, RootState, adressList } from "../../redux";
+import { AdressContainer, BaseView, BottomButtonLayout, ComponentTitle, Divider, ErrorText, Loading, Swipable } from "../../components";
+import { AppDispatch, RootState, adressList, deleteAdress } from "../../redux";
 import { Colors, Padding, Radius } from "../../constants";
 import { Adress } from "../../model";
 const { height } = Dimensions.get('window');
@@ -36,20 +36,28 @@ const AdressList: React.FC<AdressListProps> = ({ navigation }) => {
 
     return (
         <BaseView>
-            <View style={styles.innerContainer}>
-                <View style={styles.pagePadding}>
-                    {/* Title */}
-                    <ComponentTitle title={t('registered-addresses')} paddingTop={height * 0.04} />
-                    <View style={{ height: 10 }} />
-                    <View style={styles.adressListContainer}>
-                        <ScrollView showsVerticalScrollIndicator={false}>
-                            {addresses?.map((adress, index) => {
-                                return <AddressItem key={adress.id} address={adress} index={index} />
-                            })}
-                        </ScrollView>
+            {(addresses?.length ?? 0) < 1
+                ? <ErrorText error="Kayılı Adres Bulunamadı" />
+                : <View style={styles.innerContainer}>
+                    <View style={styles.pagePadding}>
+                        {/* Title */}
+                        <ComponentTitle title={t('registered-addresses')} paddingTop={height * 0.04} />
+                        <View style={{ height: 10 }} />
+                        <View style={styles.adressListContainer}>
+                            <ScrollView showsVerticalScrollIndicator={false}>
+                                {addresses?.map((adress, index) => {
+                                    return <AddressItem
+                                        key={adress.id}
+                                        address={adress}
+                                        index={index}
+                                        dispatch={dispatch}
+                                    />
+                                })}
+                            </ScrollView>
+                        </View>
                     </View>
                 </View>
-            </View>
+            }
             <BottomButtonLayout
                 title={t('add-new-record')}
                 onPress={handleAddNewRecordButton}
@@ -61,24 +69,29 @@ const AdressList: React.FC<AdressListProps> = ({ navigation }) => {
 
 interface AddressItemProps {
     address: Adress;
-    index: number
+    index: number,
+    dispatch: any;
 }
 
-const AddressItem: React.FC<AddressItemProps> = React.memo(({ address, index }) => {
+const AddressItem: React.FC<AddressItemProps> = React.memo(({ address, index, dispatch }) => {
 
     const isDividerRendered = index % 2 == 1;
 
     return <View key={address.id}>
         {isDividerRendered && <Divider />}
-        <AdressContainer
-            key={address.id}
-            adressTitle={address.adressTitle}
-            adressDetails={address.adressDescription}
-            currentAdress={address.currentAdress}
-            onPress={() => { }}
-        />
+        <Swipable onPress={() => {
+            dispatch(deleteAdress({ id: address.id!, isDeleted: true }))
+        }}>
+            <AdressContainer
+                key={address.id}
+                adressTitle={address.adressTitle}
+                adressDetails={address.adressDescription}
+                currentAdress={address.currentAdress}
+                onPress={() => { }}
+            />
+        </Swipable>
         {isDividerRendered && <Divider />}
-    </View>
+    </View >
 });
 const styles = StyleSheet.create({
     innerContainer: {
